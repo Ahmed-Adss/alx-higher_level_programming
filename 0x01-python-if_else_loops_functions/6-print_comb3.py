2e3e5807b054e4307fd35784297bb02f2b5238c8
Ahmed Ibrahim <eng.ahmedads@gmail.com>
2023-10-31 22:57:03 +0200

    0x01-python-if_else_loops_functions session

--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 for i in range(0, 10):
-    for l in range(i, 10):
-        if i == l or (i == 8 and l == 9):
+    for w in range(i, 10):
+        if i == w or (i == 8 and w == 9):
             continue
-        print("{:d}{:d}".format(i, l), end=" ,")
+        print("{:d}{:d}".format(i, w), end=" ,")
 print("{:d}{:d}".format(8, 9))
