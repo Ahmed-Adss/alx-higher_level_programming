88c21e3e4c9252d471138b9fab67f50baab6ad5d
Ahmed Ibrahim <eng.ahmedads@gmail.com>
2023-10-31 22:14:18 +0200

    0x01-python-if_else_loops_functions session

--- 0x01-python-if_else_loops_functions/2-print_alphabet.py
+++ 0x01-python-if_else_loops_functions/2-print_alphabet.py
@@ -1,3 +1,7 @@
+# #!/usr/bin/python3
+# for i in range(97, 123):
+#     print("{:c}".format(i), end="")
+
 #!/usr/bin/python3
-for i in range(97, 123):
+for i in range(ord('a'), ord('z') + 1):
     print("{:c}".format(i), end="")
