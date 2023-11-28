36b30949255c622119406afff7f659204a417b14
Ahmed Ibrahim <eng.ahmedads@gmail.com>
2023-11-27 16:18:08 -0800

    python-more_classes

---
+++ 0x08-python-more_classes/1-rectangle.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+"""Defines a Rectangle class."""
+
+
+class Rectangle:
+    """Represent a rectangle."""
+    def __init__(self, width=0, height=0):
+        """Initializes a new Rectangle instance.
+        Args:
+            width (int): The width of the rectangle.
+            height (int): The height of the rectangle.
+        """
+        self.width = width
+        self.height = height
+
+
+
+
