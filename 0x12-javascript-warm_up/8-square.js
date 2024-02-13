9634069fb5c8a5a81f47022454b75964a76ece29
Ahmed Ibrahim <eng.ahmedads@gmail.com>
2024-02-13 13:12:33 +0200

    javascript-warm_up

--- 0x12-javascript-warm_up/8-square.js
+++ 0x12-javascript-warm_up/8-square.js
@@ -1,7 +1,7 @@
 #!/usr/bin/node
 
 let xStr = '';
-while (parseInt(process.argv[2]) > 0 || parseInt(process.argv[2]) === null || parseInt(process.argv[2]) === undefined) {
+while (parseInt(process.argv[2]) > 0 || parseInt(process.argv[2]) === NaN || parseInt(process.argv[2]) === undefined) {
   if (parseInt(process.argv[2]) > 0) {
     for (let i = 0; i < process.argv[2]; i++) {
       for (let j = 0; j < process.argv[2]; j++) {
