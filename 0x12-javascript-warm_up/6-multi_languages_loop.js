191625b499292a338db80a1b5e43a19e1fd657a5
Ahmed Ibrahim <eng.ahmedads@gmail.com>
2024-02-13 05:07:33 +0200

    javascript-warm_up

--- 0x12-javascript-warm_up/6-multi_languages_loop.js
+++ 0x12-javascript-warm_up/6-multi_languages_loop.js
@@ -3,9 +3,5 @@
 const arr = ['C is fun', 'Python is cool', 'JavaScript is amazing'];
 
 for (let i = 0; i < arr.length; i++) {
-  if (i < arr.length - 1) {
-    console.log(arr[i] + '\n');
-  } else {
     console.log(arr[i]);
-  }
 }
