43afad4fb0a9e6edc724dab36e2fe3e966a36c73
Ahmed Ibrahim <eng.ahmedads@gmail.com>
2024-02-13 05:39:52 +0200

    javascript-warm_up

--- 0x12-javascript-warm_up/8-square.js
+++ 0x12-javascript-warm_up/8-square.js
@@ -1,13 +1,13 @@
 #!/usr/bin/node
 
 let xStr = '';
-if (parseInt(process.argv[2])){
-    for(let i = 0; i < process.argv[2]; i++){
-        for(let j = 0; j < process.argv[2]; j++){
-            xStr += 'X';
-        }
-        console.log(xStr);
+if (parseInt(process.argv[2])) {
+  for (let i = 0; i < process.argv[2]; i++) {
+    for (let j = 0; j < process.argv[2]; j++) {
+      xStr += 'X';
     }
-}else{
-    console.log('Missing size');
+    console.log(xStr);
+  }
+} else {
+  console.log('Missing size');
 }
