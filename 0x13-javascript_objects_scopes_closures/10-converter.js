acdc5d501f73d96c576f31b88c95f983f50a218f
Ahmed Ibrahim <eng.ahmedads@gmail.com>
2024-02-15 00:42:37 +0200

    javascript_objects_scopes_closures

--- 0x13-javascript_objects_scopes_closures/10-converter.js
+++ 0x13-javascript_objects_scopes_closures/10-converter.js
@@ -2,9 +2,9 @@
 // Converts base 10 to another base passed as argument with Closure
 
 exports.converter = function (base) {
-    function myConverter (n) {
-      return n.toString(base);
-    }
-  
-    return myConverter;
-  };
+  function myConverter (n) {
+    return n.toString(base);
+  }
+
+  return myConverter;
+};
--- 0x13-javascript_objects_scopes_closures/7-occurrences.js
+++ 0x13-javascript_objects_scopes_closures/7-occurrences.js
@@ -2,5 +2,5 @@
 // Returns the number of occurrences of an element in a list
 
 exports.nbOccurences = function (list, searchElement) {
-    return list.filter(x => x === searchElement).length;
-  };
+  return list.filter(x => x === searchElement).length;
+};
